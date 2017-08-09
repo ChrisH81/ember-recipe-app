973f851ab614bebcdd66d4fd9230caea560bfbef
ChrisH81 <chrishurst1981@icloud.com>
2017-08-09 14:14:13 +0200

    add ingredient added

---
+++ app/routes/recipes/actions.js
@@ -0,0 +1,35 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  actions: {
+    save(model){
+      model.save()
+        .then((model) => this.transitionTo('recipes.show', model))
+        .catch(e => {
+          console.log(e.errors);
+        });
+    },
+
+    willTransition() {
+
+      this.controller.get('model').rollbackAttributes();
+    },
+
+    addIngredient() {
+      this.currentModel.get('ingredients').pushObject(
+        this.get('store').createRecord('ingredient')
+      );
+    },
+    addInstruction() {
+      this.currentModel.get('instructions').pushObject(
+        this.get('store').createRecord('instruction')
+      );
+    },
+    removeIngredient(ingredient) {
+      this.currentModel.get('ingredients').removeObject(ingredient);
+    },
+    removeInstruction(instruction) {
+      this.currentModel.get('instructions').removeObject(instruction);
+    }
+  }
+});
